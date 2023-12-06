eb935f668375b412e5422f5c99e64723e0e11776
Guillermo Palma <gpalma.olea@gmail.com>
2023-12-05 21:21:30 -0300

    modifico error en getDate

--- consultas.js
+++ consultas.js
@@ -9,18 +9,16 @@ const pool = new Pool({
     allowExitOnIdle: true
 })
 
-const getDate = async () {
-    const result = await pool.query(
-        "SELECT NOW()"
-    )
-    console.log(result)
-}
+const getDate = async () => {
+    const result = await pool.query("SELECT NOW()")
+    console.log(result);
+};
 
 const agregarProducto = async (titulo, formato, imagen, precio) =>{
     const consulta = "INSERT INTO productos values (DEFAULT, $1,$2,$3,$4)"
     const values = [titulo, formato, imagen, precio]
     const result = await pool.query(consulta, values)
     console.log("¡Ya se está vendiendo!")
-}
+};
 
 module.exports = {agregarProducto}
