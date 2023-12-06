6d3d8069366265a6b29cb20df751773e32f4946a
Guillermo Palma <gpalma.olea@gmail.com>
2023-12-06 20:11:10 -0300

    update de plantilla back

--- consultas.js
+++ consultas.js
@@ -5,14 +5,23 @@ const pool = new Pool({
     host: 'localhost',
     user: 'postgres',
     password: 'postgres',
-    database: 'catalogo_productos',
+    database: 'retrogroove',
     allowExitOnIdle: true
 })
+
+5432
 
 const getDate = async () => {
     const result = await pool.query("SELECT NOW()")
     console.log(result);
 };
+
+const verificarCredenciales = async (email, password) =>{
+    const consulta = "Select * FROM usuarios WHERE email = $1 AND password = $2"
+    const values = [email, password]
+    const {rowCount} = await pool.query(consulta, values)
+    if (!rowCount) throw {code: 404, nessage: "No se encontró ningún usuario con estas credenciaels"} 
+}
 
 const agregarProducto = async (titulo, formato, imagen, precio) =>{
     const consulta = "INSERT INTO productos values (DEFAULT, $1,$2,$3,$4)"
@@ -21,4 +30,8 @@ const agregarProducto = async (titulo, formato, imagen, precio) =>{
     console.log("¡Ya se está vendiendo!")
 };
 
-module.exports = {agregarProducto}
+const consultarProducto = async (titulo, formato, imagen, precio) =>{
+    const consulta = "SELECT "
+}
+
+module.exports = {agregarProducto, verificarCredenciales, consultarProducto}
--- index.js
+++ index.js
@@ -1,46 +1,61 @@
-const {agregarProducto, } = require('./consultas')
+const {
+  agregarProducto,
+  verificarCredenciales,
+  consultarProducto,
+} = require("./consultas.js");
+
 const express = require("express");
 const app = express();
 const fs = require("fs");
+const cors = require("cors");
+
 PORT = 3000;
 
 app.use(express.json());
 app.use(cors());
 
-// login 
-app.post('/', async (req,res){
-    try {
-        
-    } catch (error) {
-        
-    }
-}) 
+// login
+app.post("/", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    await verificarCredenciales(email, password);
+    const token = jwt.sing({ email }, "az_AZ");
+    res.send(token);
+  } catch (error) {
+    console.log(error);
+    res.status(error.code || 500).send(error);
+  }
+});
 
 // landing
-app.get('/landing', async (req,res){
-    try {
-        
-    } catch (error) {
-        
-    }
-}) 
+app.get("/landing", async (req, res) => {
+  try {
+  } catch (error) {}
+});
 
 // producto
-app.get('/producto:id', async (req,res){
-    try {
-        
-    } catch (error) {
-        
-    }
-}) 
+app.get("/producto:titulo", async (req, res) => {
+  try {
+    const { titulo } = req.params;
+    const consultarProducto = productos.find(
+      (producto) => producto.titulo === titulo
+    );
+    is(consultarProducto);
+    return res.json(movie);
+  } catch (error) {
+    res.status(404).send("Producto no encontrado");
+  }
+});
 // vender
-app.post('/vender', async (req,res){
-    try {
-        
-    } catch (error) {
-        
-    }
-}) 
+app.post("/vender", async (req, res) => {
+  try {
+    const { titulo, formato, imagen, precio } = req.body;
+    await agregarProducto(titulo, formato, imagen, precio);
+    res.send("¡Producto agregado!");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error interno del servidor");
+  }
+});
 
-
-app.listen(PORT, console.log("¡Servidor ON!"));
+app.listen(PORT, console.log(`¡Servidor ON en el puerto: ${port}!`));
